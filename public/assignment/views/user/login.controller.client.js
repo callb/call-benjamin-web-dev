7076b782b3aac742ec881982fb2e43b7139d4709
Ben Call <benjamin.ncall@gmail.com>
2016-06-19 18:43:14 -0400

    validation

--- public/assignment/views/user/login.controller.client.js
+++ public/assignment/views/user/login.controller.client.js
@@ -10,22 +10,28 @@
         vm.login = login;
 
         function login (username, password) {
-            UserService
-                .login(username, password)
-                .then(
-                    function(response) {
-                        console.log(response);
-                        var user = response.data;
-                        if(user) {
-                            $rootScope.currentUser = user;
-                            var id = user._id;
-                            $location.url("/user/" + id);
+            if(username == null || password == null) {
+                vm.error = "username and password required";
+
+            } else {
+                UserService
+                    .login(username, password)
+                    .then(
+                        function(response) {
+                            console.log(response);
+                            var user = response.data;
+                            if(user) {
+                                $rootScope.currentUser = user;
+                                var id = user._id;
+                                $location.url("/user/" + id);
+                            }
+                        },
+                        function (error) {
+                            vm.error = "User not found";
                         }
-                    },
-                    function (error) {
-                        vm.error = "User not found";
-                    }
-                );
+                    );
+            }
+
         }
     }
 })();
