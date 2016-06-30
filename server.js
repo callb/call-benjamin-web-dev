712326a4493f5075ea611138a1a2992e365fd937
Ben Call <benjamin.ncall@gmail.com>
2016-06-30 10:25:01 -0400

    server changes to listening

--- server.js
+++ server.js
@@ -25,7 +25,18 @@ project(app);
 var assignment = require('./assignment/app.js');
 assignment(app);
 
-var ipaddress = process.env.OPENSHIFT_NODEJS_IP;
-var port      = process.env.OPENSHIFT_NODEJS_PORT || 3000;
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+
+var db_name = "cs4550summer1";
+//provide a sensible default for local development
+mongodb_connection_string = 'mongodb://127.0.0.1:27017/' + db_name;
+//take advantage of openshift env vars when available:
+if(process.env.OPENSHIFT_MONGODB_DB_URL){
+    mongodb_connection_string = process.env.OPENSHIFT_MONGODB_DB_URL + db_name;
+}
+
+
+app.listen(server_port, server_ip_address);
 
 app.listen(port, ipaddress);
