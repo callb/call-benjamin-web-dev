a33830c32aca5c1475c52ab2067a73f716408cf9
Ben Call <benjamin.ncall@gmail.com>
2016-06-30 17:08:03 -0400

    changed ports back

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 
 var db_name = "cs4550summer1";
 //provide a sensible default for local development
-mongodb_connection_string = 'mongodb://127.0.0.1:27018/' + db_name;
+mongodb_connection_string = 'mongodb://127.0.0.1:27017/' + db_name;
 //take advantage of openshift env vars when available:
 if(process.env.OPENSHIFT_MONGODB_DB_URL){
     mongodb_connection_string = process.env.OPENSHIFT_MONGODB_DB_URL + db_name;
